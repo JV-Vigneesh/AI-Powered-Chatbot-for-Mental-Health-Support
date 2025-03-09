1f9174a65c0b70761244778cf22a7574c956b01e
JV Vigneesh <108445978+JV-Vigneesh@users.noreply.github.com>
2025-03-09 16:11:24 +0530

    Delete src/main directory

--- src/main/java/com/chatbot/repository/ChatRepository.java
+++
@@ -1,8 +0,0 @@
-package com.chatbot.repository;
-
-import com.chatbot.model.ChatMessage;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ChatRepository extends JpaRepository<ChatMessage, Long> {}
